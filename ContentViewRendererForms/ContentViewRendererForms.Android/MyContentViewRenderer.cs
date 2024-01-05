b9e46f2dc21a1e7c3243db1c21a1c9dc28a7d977
Kevin Anderson <kraatw2008@yahoo.com>
2024-01-05 00:29:41 -0600

    hello

---
+++ ContentViewRendererForms/ContentViewRendererForms.Android/MyContentViewRenderer.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using Android.Graphics;
+using Android.Views;
+using ContentViewRendererForms;
+using ContentViewRendererForms.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using View = Android.Views.View;
+
+[assembly: ExportRenderer(typeof(MyContentView), typeof(MyContentViewRenderer))]
+namespace ContentViewRendererForms.Droid
+{
+    public class MyContentViewRenderer : ViewRenderer<MyContentView, View>,
+        TextureView.ISurfaceTextureListener
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<MyContentView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var textureView = new TextureView(Context);
+                textureView.SurfaceTextureListener = this;
+
+                AddView(textureView);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+        }
+
+        public void OnSurfaceTextureAvailable(SurfaceTexture surface, int width, int height)
+        {
+            System.Diagnostics.Debug.WriteLine("### the SurfaceTexture has become available!");
+            Debugger.Break();
+        }
+
+        public bool OnSurfaceTextureDestroyed(SurfaceTexture surface)
+        {
+            return true;
+        }
+
+        public void OnSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height)
+        {
+        }
+
+        public void OnSurfaceTextureUpdated(SurfaceTexture surface)
+        {
+        }
+    }
+}
